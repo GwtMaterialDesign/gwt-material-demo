c5cfc2e03d73498b0f93cdd8c683b55ec64df152
BenDol <dolb90@gmail.com>
2015-11-09 22:54:51 +1300

    Remove .iml file.

--- src/main/java/gwt/material/design/demo/client/application/components/badges/BadgesModule.java
+++ src/main/java/gwt/material/design/demo/client/application/components/badges/BadgesModule.java
@@ -5,7 +5,7 @@ import com.gwtplatform.mvp.client.gin.AbstractPresenterModule;
 public class BadgesModule extends AbstractPresenterModule {
     @Override
     protected void configure() {
-        bindPresenter(BadgesPresenter.class, BadgesPresenter.MyView.class,
-            BadgesView.class, BadgesPresenter.MyProxy.class);
+        bindPresenter(BadgesPresenter.class, BadgesPresenter.MyView.class, BadgesView.class,
+            BadgesPresenter.MyProxy.class);
     }
 }
