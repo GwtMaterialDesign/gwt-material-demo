211aa66c1c23dc682728b1bfc2379150e9122e05
Kevzlou7979 <kevzlou7979@gmail.com>
2015-11-21 00:55:03 +0800

    Updated MaterialNavBar and Added Collapsible Progress Showcase.

--- src/main/java/gwt/material/design/demo/client/application/components/loader/LoaderView.java
+++ src/main/java/gwt/material/design/demo/client/application/components/loader/LoaderView.java
@@ -5,8 +5,11 @@ import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.uibinder.client.UiHandler;
 import com.google.gwt.user.client.Timer;
+import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Widget;
 import com.gwtplatform.mvp.client.ViewImpl;
+import gwt.material.design.client.constants.ProgressType;
+import gwt.material.design.client.ui.MaterialCollapsibleItem;
 import gwt.material.design.client.ui.MaterialLoader;
 import gwt.material.design.client.ui.MaterialNavBar;
 
@@ -16,7 +19,11 @@ public class LoaderView extends ViewImpl implements LoaderPresenter.MyView {
     interface Binder extends UiBinder<Widget, LoaderView> {
     }
 
-    @UiField MaterialNavBar navBar;
+    @UiField
+    MaterialNavBar navBar;
+
+    @UiField
+    MaterialCollapsibleItem collapseItemOne, collapseItemTwo, collapseItemThree;
 
     @Inject
     LoaderView(Binder uiBinder) {
@@ -49,13 +56,50 @@ public class LoaderView extends ViewImpl implements LoaderPresenter.MyView {
 
     @UiHandler("btnShowNavBarProgress")
     void onShowNavBarProgress(ClickEvent e) {
-        navBar.showLoader();
+        navBar.showProgress(ProgressType.INDETERMINATE);
         Timer t = new Timer() {
             @Override
             public void run() {
-                navBar.hideLoader();
+                navBar.hideProgress();
             }
         };
         t.schedule(3000);
     }
+
+    @UiHandler("collapseItemOne")
+    void onShowCollapseProgress(ClickEvent e){
+        collapseItemOne.showProgress(ProgressType.INDETERMINATE);
+        Timer t = new Timer() {
+            @Override
+            public void run() {
+                collapseItemOne.hideProgress();
+            }
+        };
+        t.schedule(3000);
+    }
+
+    @UiHandler("collapseItemTwo")
+    void onShowCollapseTwoProgress(ClickEvent e){
+        collapseItemTwo.showProgress(ProgressType.INDETERMINATE);
+        Timer t = new Timer() {
+            @Override
+            public void run() {
+                collapseItemTwo.hideProgress();
+            }
+        };
+        t.schedule(3000);
+    }
+
+    @UiHandler("collapseItemThree")
+    void onShowCollapseThreeProgress(ClickEvent e){
+        collapseItemThree.showProgress(ProgressType.INDETERMINATE);
+        Timer t = new Timer() {
+            @Override
+            public void run() {
+                collapseItemThree.hideProgress();
+            }
+        };
+        t.schedule(3000);
+    }
+
 }
