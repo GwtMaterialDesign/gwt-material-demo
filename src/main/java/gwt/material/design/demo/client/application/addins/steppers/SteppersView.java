e7e7eba0190d3642b277ab1bd2112d3f6ccef9fa
Kevzlou7979 <kevzlou7979@gmail.com>
2015-12-03 16:53:33 +0800

    Added Modal Steppers Showcase.

--- src/main/java/gwt/material/design/demo/client/application/addins/steppers/SteppersView.java
+++ src/main/java/gwt/material/design/demo/client/application/addins/steppers/SteppersView.java
@@ -7,6 +7,7 @@ import com.google.gwt.uibinder.client.UiHandler;
 import com.google.gwt.user.client.ui.Widget;
 import com.gwtplatform.mvp.client.ViewImpl;
 import gwt.material.design.addins.client.ui.MaterialStepper;
+import gwt.material.design.client.ui.MaterialModal;
 import gwt.material.design.client.ui.MaterialToast;
 
 import javax.inject.Inject;
@@ -17,7 +18,10 @@ public class SteppersView extends ViewImpl implements SteppersPresenter.MyView {
     }
 
     @UiField
-    MaterialStepper stepper, stepperCard;
+    MaterialStepper stepper, stepperCard, stepperModal;
+
+    @UiField
+    MaterialModal modalStepper;
 
     @Inject
     SteppersView(Binder uiBinder) {
@@ -40,19 +44,40 @@ public class SteppersView extends ViewImpl implements SteppersPresenter.MyView {
         stepper.goToStep(1);
     }
 
-    @UiHandler({"btnContinue11", "btnContinue21", "btnContinue31"})
+    @UiHandler({"btnContinue11", "btnContinue12", "btnContinue13"})
     void onNextStep1(ClickEvent e){
         stepperCard.nextStep();
     }
 
-    @UiHandler({"btnPrev11", "btnPrev21", "btnPrev31"})
+    @UiHandler({"btnPrev11", "btnPrev12", "btnPrev13"})
     void onPrevStep1(ClickEvent e){
         stepperCard.prevStep();
     }
 
-    @UiHandler("btnContinue31")
+    @UiHandler("btnContinue13")
     void onFinish1(ClickEvent e){
         MaterialToast.fireToast("All done.");
         stepperCard.goToStep(1);
+    }
+
+    @UiHandler("btnModal")
+    void onOpenModal(ClickEvent e){
+        modalStepper.openModal();
+    }
+
+    @UiHandler({"btnContinue31", "btnContinue32", "btnContinue33"})
+    void onNextStep2(ClickEvent e){
+        stepperModal.nextStep();
+    }
+
+    @UiHandler({"btnPrev31", "btnPrev32", "btnPrev33"})
+    void onPrevStep2(ClickEvent e){
+        stepperModal.prevStep();
+    }
+
+    @UiHandler("btnContinue33")
+    void onFinish3(ClickEvent e){
+        MaterialToast.fireToast("All done.");
+        modalStepper.closeModal();
     }
 }
