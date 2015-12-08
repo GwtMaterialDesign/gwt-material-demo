7cec91b974531df568e663d8c6d835db62f318ba
Kevzlou7979 <kevzlou7979@gmail.com>
2015-12-08 21:19:39 +0800

    Material Path Animator Showcase and Code Blocks Step Guide about Source and Target Components
    Path Animator : Grid Showcase and Code Blocks
    Path Animator : Card to Overlay Panel Showcase
    Path Animator : Button to Card Showcase

--- src/main/java/gwt/material/design/demo/client/application/addins/pathanimator/PathAnimatorView.java
+++ src/main/java/gwt/material/design/demo/client/application/addins/pathanimator/PathAnimatorView.java
@@ -9,6 +9,8 @@ import com.google.gwt.user.client.ui.Widget;
 import com.gwtplatform.mvp.client.ViewImpl;
 import gwt.material.design.addins.client.ui.MaterialPathAnimator;
 import gwt.material.design.client.ui.MaterialButton;
+import gwt.material.design.client.ui.MaterialCard;
+import gwt.material.design.client.ui.MaterialLink;
 import gwt.material.design.client.ui.MaterialPanel;
 
 import javax.inject.Inject;
@@ -19,10 +21,16 @@ public class PathAnimatorView extends ViewImpl implements PathAnimatorPresenter.
     }
 
     @UiField
-    MaterialButton btnSource;
+    MaterialButton btnSource1, btnSource3;
 
     @UiField
-    MaterialPanel panelTarget;
+    MaterialCard card, panelTarget3;
+
+    @UiField
+    MaterialLink btnSource2;
+
+    @UiField
+    MaterialPanel panelTarget1, panelTarget2;
 
     @Inject
     PathAnimatorView(Binder uiBinder) {
@@ -30,22 +38,60 @@ public class PathAnimatorView extends ViewImpl implements PathAnimatorPresenter.
     }
 
 
-    @UiHandler("btnSource")
-    void onSource(ClickEvent e){
+    @UiHandler("btnSource1")
+    void onSource(ClickEvent e) {
         Runnable callback = new Runnable() {
             @Override
             public void run() {
-                panelTarget.getElement().getStyle().setVisibility(Style.Visibility.VISIBLE);
-                panelTarget.setOpacity(1);
+                panelTarget1.getElement().getStyle().setVisibility(Style.Visibility.VISIBLE);
+                panelTarget1.setOpacity(1);
             }
         };
-        MaterialPathAnimator.animate(btnSource.getElement(), panelTarget.getElement(), callback);
+        MaterialPathAnimator.animate(btnSource1.getElement(), panelTarget1.getElement(), callback);
     }
 
-    @UiHandler("btnClose")
-    void onClose(ClickEvent e){
-        panelTarget.getElement().getStyle().setVisibility(Style.Visibility.HIDDEN);
-        panelTarget.setOpacity(0);
-        MaterialPathAnimator.animate(panelTarget.getElement(), btnSource.getElement());
+    @UiHandler("btnSource2")
+    void onClickCard(ClickEvent e) {
+        Runnable callback = new Runnable() {
+            @Override
+            public void run() {
+                panelTarget2.getElement().getStyle().setVisibility(Style.Visibility.VISIBLE);
+                panelTarget2.setOpacity(1);
+            }
+        };
+        MaterialPathAnimator.animate(card.getElement(), panelTarget2.getElement(), callback);
+    }
+
+    @UiHandler("btnSource3")
+    void onClickCard3(ClickEvent e) {
+        Runnable callback = new Runnable() {
+            @Override
+            public void run() {
+                panelTarget3.getElement().getStyle().setVisibility(Style.Visibility.VISIBLE);
+                panelTarget3.setOpacity(1);
+            }
+        };
+        MaterialPathAnimator.animate(btnSource3.getElement(), panelTarget3.getElement(), callback);
+    }
+
+    @UiHandler("btnClose1")
+    void onClose(ClickEvent e) {
+        panelTarget1.getElement().getStyle().setVisibility(Style.Visibility.HIDDEN);
+        panelTarget1.setOpacity(0);
+        MaterialPathAnimator.animate(panelTarget1.getElement(), btnSource1.getElement());
+    }
+
+    @UiHandler("btnClose2")
+    void onCardClose(ClickEvent e) {
+        panelTarget2.getElement().getStyle().setVisibility(Style.Visibility.HIDDEN);
+        panelTarget2.setOpacity(0);
+        MaterialPathAnimator.animate(panelTarget2.getElement(), btnSource2.getElement());
+    }
+
+    @UiHandler("btnClose3")
+    void onCardClose3(ClickEvent e) {
+        panelTarget3.getElement().getStyle().setVisibility(Style.Visibility.HIDDEN);
+        panelTarget3.setOpacity(0);
+        MaterialPathAnimator.animate(panelTarget3.getElement(), btnSource3.getElement());
     }
 }
