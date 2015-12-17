f53fd31cce956b9f5e6aefdde34e7357224172a8
Kevzlou7979 <kevzlou7979@gmail.com>
2015-12-17 08:43:39 +0800

    Material Showcase : Added custom Format Showcase and Code blocks

--- src/main/java/gwt/material/design/demo/client/application/components/pickers/PickersView.java
+++ src/main/java/gwt/material/design/demo/client/application/components/pickers/PickersView.java
@@ -1,17 +1,41 @@
 package gwt.material.design.demo.client.application.components.pickers;
 
+import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.uibinder.client.UiHandler;
 import com.google.gwt.user.client.ui.Widget;
 import com.gwtplatform.mvp.client.ViewImpl;
+import gwt.material.design.client.ui.MaterialDatePicker;
+import gwt.material.design.client.ui.MaterialToast;
 
 import javax.inject.Inject;
+import java.util.Date;
 
 public class PickersView extends ViewImpl implements PickersPresenter.MyView {
     interface Binder extends UiBinder<Widget, PickersView> {
     }
 
+    @UiField
+    MaterialDatePicker dp, dpFormat;
+
     @Inject
     PickersView(Binder uiBinder) {
         initWidget(uiBinder.createAndBindUi(this));
+    }
+
+    @UiHandler("btnSetDate")
+    void onSetDate(ClickEvent e){
+        dp.setDate(new Date(116, 0,1));
+    }
+
+    @UiHandler("btnGetDate")
+    void onGetDate(ClickEvent e){
+        MaterialToast.fireToast("" + dp.getDate());
+    }
+
+    @UiHandler("btnSetFormat")
+    void onDpFormat(ClickEvent e){
+        dpFormat.setDate(new Date());
     }
 }
