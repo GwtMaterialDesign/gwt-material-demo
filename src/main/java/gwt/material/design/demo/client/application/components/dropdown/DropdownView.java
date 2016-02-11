c471d71c1e5fac952324e7af1d0ecf9035b91f78
Kevzlou7979 <kevzlou7979@gmail.com>
2016-02-11 20:00:41 +0800

    Added showcase for selection event on dropdown component

--- src/main/java/gwt/material/design/demo/client/application/components/dropdown/DropdownView.java
+++ src/main/java/gwt/material/design/demo/client/application/components/dropdown/DropdownView.java
@@ -1,8 +1,12 @@
 package gwt.material.design.demo.client.application.components.dropdown;
 
+import com.google.gwt.event.logical.shared.SelectionEvent;
 import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiHandler;
 import com.google.gwt.user.client.ui.Widget;
 import com.gwtplatform.mvp.client.ViewImpl;
+import gwt.material.design.client.ui.MaterialLink;
+import gwt.material.design.client.ui.MaterialToast;
 
 import javax.inject.Inject;
 
@@ -14,4 +18,10 @@ public class DropdownView extends ViewImpl implements DropdownPresenter.MyView {
     DropdownView(Binder uiBinder) {
         initWidget(uiBinder.createAndBindUi(this));
     }
+
+    @UiHandler("dropdown")
+    void onDropdown(SelectionEvent<Widget> callback){
+        MaterialToast.fireToast("Selected : " + ((MaterialLink)callback.getSelectedItem()).getText());
+    }
+
 }
