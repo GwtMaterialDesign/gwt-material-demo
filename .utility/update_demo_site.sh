c6dcf849658d7d5c6f498bba2db5e01c8e7a8c4a
Kevzlou7979 <kevzlou7979@gmail.com>
2016-07-21 16:39:21 +0800

    Readd travis.

---
+++ .utility/update_demo_site.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+set -ev
+if [ "$TRAVIS_JDK_VERSION" == "oraclejdk8" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "release_2.0" ]; then
+
+if [[ -z "$GH_TOKEN" ]]; then
+echo -e "GH_TOKEN is not set"
+exit 1
+fi
+
+if [ ! -f $TRAVIS_BUILD_DIR/target/gwt-material-demo-*.war ]; then
+echo -e "demo war file not found."
+exit 1
+fi
+
+echo -e "Publishing demo to gh-pages . . .\n"
+
+git config --global user.email "travis@travis-ci.org"
+git config --global user.name "travis-ci"
+
+# clone the gh-pages branch.
+cd $HOME
+rm -rf gh-pages
+git clone --quiet --branch=gh-pages https://$GH_TOKEN@github.com/GwtMaterialDesign/gwt-material-demo gh-pages > /dev/null
+cd gh-pages
+
+# remove the GwtMaterialDemo directories from git.
+if [[ -d ./2.0-SNAPSHOT/gwtmaterialdemo ]]; then
+git rm -rf ./2.0-SNAPSHOT/gwtmaterialdemo
+fi
+if [[ -f ./2.0-SNAPSHOT/index.html ]]; then
+git rm -rf ./2.0-SNAPSHOT/index.html
+fi
+if [[ -d ./2.0-SNAPSHOT/META-INF ]]; then
+git rm -rf ./2.0-SNAPSHOT/META-INF
+fi
+if [[ -d ./2.0-SNAPSHOT/WEB-INF ]]; then
+git rm -rf ./2.0-SNAPSHOT/WEB-INF
+fi
+
+# copy the new GwtMaterialDemo the 2.0-SNAPSHOT dir.
+unzip -u $TRAVIS_BUILD_DIR/target/gwt-material-demo-*.war -d ./2.0-SNAPSHOT/
+rm -rf ./2.0-SNAPSHOT/META-INF
+rm -rf ./2.0-SNAPSHOT/WEB-INF
+
+git add -f .
+git commit -m "Auto-push demo to gh-pages successful. (Travis build: $TRAVIS_BUILD_NUMBER)"
+git push -fq origin gh-pages > /dev/null
+
+echo -e "Published demo to gh-pages.\n"
+
+fi
