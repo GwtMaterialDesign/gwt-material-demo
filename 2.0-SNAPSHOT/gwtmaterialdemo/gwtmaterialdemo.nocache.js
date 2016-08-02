d121b518b29bb1f07c19e8d6240c0769eb9fa635
travis-ci <travis@travis-ci.org>
2016-08-02 11:25:30 +0000

    Auto-push demo to gh-pages successful. (Travis build: 264)

--- 2.0-SNAPSHOT/gwtmaterialdemo/gwtmaterialdemo.nocache.js
+++ 2.0-SNAPSHOT/gwtmaterialdemo/gwtmaterialdemo.nocache.js
@@ -334,7 +334,7 @@ function gwtmaterialdemo(){
     }
     var strongName;
     try {
-      strongName = 'EBF2F03C45040BAFCE2671489BA49846';
+      strongName = '96466EFE139B239431B2F3E5933C4AE3';
       var idx = strongName.indexOf(':');
       if (idx != -1) {
         softPermutationId = parseInt(strongName.substring(idx + 1), 10);
