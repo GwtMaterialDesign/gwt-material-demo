9e91c88af394d3bf18da0f5c6c5d1a13d621bc73
Kevzlou7979 <kevzlou7979@gmail.com>
2016-10-30 15:38:28 +0800

    Preparation for 2.0-rc1 release

--- .utility/update_demo_site.sh
+++ .utility/update_demo_site.sh
@@ -24,23 +24,23 @@ git clone --quiet --branch=gh-pages https://$GH_TOKEN@github.com/GwtMaterialDesi
 cd gh-pages
 
 # remove the GwtMaterialDemo directories from git.
-if [[ -d ./2.0-SNAPSHOT/gwtmaterialdemo ]]; then
-git rm -rf ./2.0-SNAPSHOT/gwtmaterialdemo
+if [[ -d ./gwtmaterialdemo ]]; then
+git rm -rf ./gwtmaterialdemo
 fi
-if [[ -f ./2.0-SNAPSHOT/index.html ]]; then
-git rm -rf ./2.0-SNAPSHOT/index.html
+if [[ -f ./index.html ]]; then
+git rm -rf ./index.html
 fi
-if [[ -d ./2.0-SNAPSHOT/META-INF ]]; then
-git rm -rf ./2.0-SNAPSHOT/META-INF
+if [[ -d ./META-INF ]]; then
+git rm -rf ./META-INF
 fi
-if [[ -d ./2.0-SNAPSHOT/WEB-INF ]]; then
-git rm -rf ./2.0-SNAPSHOT/WEB-INF
+if [[ -d ./WEB-INF ]]; then
+git rm -rf ./WEB-INF
 fi
 
 # copy the new GwtMaterialDemo the 2.0-SNAPSHOT dir.
-unzip -u $TRAVIS_BUILD_DIR/target/gwt-material-demo-*.war -d ./2.0-SNAPSHOT/
-rm -rf ./2.0-SNAPSHOT/META-INF
-rm -rf ./2.0-SNAPSHOT/WEB-INF
+unzip -u $TRAVIS_BUILD_DIR/target/gwt-material-demo-*.war -d ./
+rm -rf ./META-INF
+rm -rf ./WEB-INF
 
 git add -f .
 git commit -m "Auto-push demo to gh-pages successful. (Travis build: $TRAVIS_BUILD_NUMBER)"
