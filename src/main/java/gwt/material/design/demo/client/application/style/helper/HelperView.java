1341fe5ac80cef9cbe06fc98c0f94c17afd83d32
Kevzlou7979 <kevzlou7979@gmail.com>
2017-01-16 17:31:47 +0800

    Fixed issues on latest NavBrand updates.
    Added ViewPortHelper demo showcase and codeblocks.

--- src/main/java/gwt/material/design/demo/client/application/style/helper/HelperView.java
+++ src/main/java/gwt/material/design/demo/client/application/style/helper/HelperView.java
@@ -21,9 +21,16 @@ package gwt.material.design.demo.client.application.style.helper;
  */
 
 
+import com.google.gwt.core.client.GWT;
 import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.ui.Widget;
 import com.gwtplatform.mvp.client.ViewImpl;
+import gwt.material.design.client.base.helper.ViewPortHelper;
+import gwt.material.design.client.constants.IconType;
+import gwt.material.design.client.ui.MaterialLabel;
+import com.google.gwt.user.client.Window;
+import gwt.material.design.client.ui.MaterialLink;
 
 import javax.inject.Inject;
 
@@ -31,8 +38,46 @@ public class HelperView extends ViewImpl implements HelperPresenter.MyView {
     interface Binder extends UiBinder<Widget, HelperView> {
     }
 
+    @UiField
+    MaterialLink lblViewPort;
+
     @Inject
     HelperView(Binder uiBinder) {
         initWidget(uiBinder.createAndBindUi(this));
+        detectViewPort();
+        Window.addResizeHandler(resizeEvent -> {
+            detectViewPort();
+        });
+    }
+
+    protected void detectViewPort() {
+        if (ViewPortHelper.matchMedia(ViewPortHelper.ViewPort.MOBILE_SMALL)) {
+            lblViewPort.setText("ViewPort : Mobile Small");
+            lblViewPort.setIconType(IconType.PHONE_ANDROID);
+        }
+        if (ViewPortHelper.matchMedia(ViewPortHelper.ViewPort.MOBILE_MEDIUM)) {
+            lblViewPort.setText("ViewPort : Mobile Medium");
+            lblViewPort.setIconType(IconType.PHONE_ANDROID);
+        }
+        if (ViewPortHelper.matchMedia(ViewPortHelper.ViewPort.MOBILE_LARGE)) {
+            lblViewPort.setText("ViewPort : Mobile Large");
+            lblViewPort.setIconType(IconType.PHONE_ANDROID);
+        }
+        if (ViewPortHelper.matchMedia(ViewPortHelper.ViewPort.TABLET)) {
+            lblViewPort.setText("ViewPort : Tablet");
+            lblViewPort.setIconType(IconType.TABLET_ANDROID);
+        }
+        if (ViewPortHelper.matchMedia(ViewPortHelper.ViewPort.LAPTOP)) {
+            lblViewPort.setText("ViewPort : Laptop");
+            lblViewPort.setIconType(IconType.LAPTOP);
+        }
+        if (ViewPortHelper.matchMedia(ViewPortHelper.ViewPort.LAPTOP_LARGE)) {
+            lblViewPort.setText("ViewPort : Laptop Large");
+            lblViewPort.setIconType(IconType.LAPTOP);
+        }
+        if (ViewPortHelper.matchMedia(ViewPortHelper.ViewPort.LAPTOP_4K)) {
+            lblViewPort.setText("ViewPort : Laptop 4K");
+            lblViewPort.setIconType(IconType.LAPTOP);
+        }
     }
 }
