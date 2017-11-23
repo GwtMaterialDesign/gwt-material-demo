792338e953bed88e4a6b4a9cc7a85a4f03bb28e2
Mark Kevin Baldemor <kevzlou7979@gmail.com>
2017-11-23 14:05:51 +0800

    Update gitignore to include /bin

---
+++ bin/sw-template.js
@@ -0,0 +1,46 @@
+var cacheName = '{{cacheName}}';
+var filesToCache = [  {{#each filesToCache}}
+'{{.}}'{{#unless @last}},{{/unless}}
+    {{/each}}
+    ];
+
+
+self.addEventListener('install', function (e) {
+    console.log('[ServiceWorker] Install');
+    e.waitUntil(
+        caches.open(cacheName).then(function (cache) {
+            console.log('[ServiceWorker] Caching app shell');
+            return cache.addAll(filesToCache);
+        })
+    );
+});
+
+        
+self.addEventListener('activate', function (e) {
+    console.log('[ServiceWorker] Activate');
+    e.waitUntil(
+        caches.keys().then(function (keyList) {
+            return Promise.all(keyList.map(function (key) {
+                console.log('[ServiceWorker] Removing old cache', key);
+                if (key !== cacheName) {
+                    return caches.delete(key);
+                }
+            }));
+        })
+    );
+});
+        
+self.addEventListener('fetch', function (e) {
+    console.log('[ServiceWorker] Fetch', e.request.url);
+    e.respondWith(
+        caches.match(e.request).then(function (response) {
+            return response || fetch(e.request);
+        })
+    );
+});
+        
+self.addEventListener('message', function (e) {
+    if (e.data == 'skipWaiting') {
+        self.skipWaiting();
+    }
+});
