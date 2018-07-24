6d8ec4a1bc5f5952ccab0a9bed88bfcb5f4af10f
mark.baldemor <kevzlou7979@gmail.com>
2018-07-24 19:00:40 +0800

    More updates after 2.1 release

--- src/main/java/gwt/material/design/demo/client/application/pwa/notification/NotificationView.java
+++ src/main/java/gwt/material/design/demo/client/application/pwa/notification/NotificationView.java
@@ -21,9 +21,15 @@ package gwt.material.design.demo.client.application.pwa.notification;
  */
 
 
+import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiHandler;
 import com.google.gwt.user.client.ui.Widget;
 import com.gwtplatform.mvp.client.ViewImpl;
+import gwt.material.design.client.pwa.PwaManager;
+import gwt.material.design.client.pwa.push.js.Notification;
+import gwt.material.design.client.pwa.push.js.NotificationOptions;
+import gwt.material.design.client.ui.MaterialToast;
 
 import javax.inject.Inject;
 
@@ -34,5 +40,40 @@ public class NotificationView extends ViewImpl implements NotificationPresenter.
     @Inject
     NotificationView(Binder uiBinder) {
         initWidget(uiBinder.createAndBindUi(this));
+    }
+
+    @UiHandler("requestPermission")
+    void requestPermission(ClickEvent e) {
+        if (isSupported()) {
+            Notification.requestPermission(status -> MaterialToast.fireToast("Status: " + status));
+        }
+    }
+
+    @UiHandler("notify")
+    void notify(ClickEvent e) {
+        if (isSupported()) {
+            if (Notification.getPermission().equals("granted")) {
+                NotificationOptions options = new NotificationOptions();
+                options.body = "I love GMD";
+                options.icon = "https://user.oc-static.com/upload/2017/05/03/14938342186053_01-duration-and-easing.png";
+                // Will show the Notification provided by NotificationOptions
+                Notification notification = new Notification("GMD Says", options);
+                // Listen to any Notification events
+                notification.setOnclick(param1 -> MaterialToast.fireToast("Clicked"));
+                notification.setOnclose(param1 -> MaterialToast.fireToast("Closed"));
+                notification.setOnerror(param1 -> MaterialToast.fireToast("Error"));
+                notification.setOnshow(param1 -> MaterialToast.fireToast("Shown"));
+            } else {
+                MaterialToast.fireToast("Permission Denied. Update it thru the browser setting");
+            }
+        }
+    }
+
+    protected boolean isSupported() {
+        boolean supported = PwaManager.isPwaSupported();
+        if (!supported) {
+            MaterialToast.fireToast("Push Notification is not supported");
+        }
+        return supported;
     }
 }
